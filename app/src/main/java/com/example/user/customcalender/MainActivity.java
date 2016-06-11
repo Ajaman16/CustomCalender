389c947f122cd8f8f14db2c3fa512029371336e9
Ajaman16 <ajaman16@gmail.com>
2016-06-11 12:46:10 +0530

    Custom Calender

---
+++ app/src/main/java/com/example/user/customcalender/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.user.customcalender;
+
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.imanoweb.calendarview.CalendarListener;
+import com.imanoweb.calendarview.CustomCalendarView;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Initialize CustomCalendarView from layout
+        CustomCalendarView calendarView = (CustomCalendarView) findViewById(R.id.calendar_view);
+
+        //Initialize calendar with date
+        Calendar currentCalendar = Calendar.getInstance(Locale.getDefault());
+
+        //Show Monday as first date of week
+        calendarView.setFirstDayOfWeek(Calendar.MONDAY);
+
+        //Show/hide overflow days of a month
+        calendarView.setShowOverflowDate(false);
+
+        //call refreshCalendar to update calendar the view
+        calendarView.refreshCalendar(currentCalendar);
+
+        //Handling custom calendar events
+        calendarView.setCalendarListener(new CalendarListener() {
+            @Override
+            public void onDateSelected(Date date) {
+                SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy");
+                Toast.makeText(MainActivity.this, df.format(date), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onMonthChanged(Date date) {
+                SimpleDateFormat df = new SimpleDateFormat("MM-yyyy");
+                Toast.makeText(MainActivity.this, df.format(date), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //Setting custom font
+        /*final Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/Arch_Rival_Bold.ttf");
+        if (null != typeface) {
+            calendarView.setCustomTypeface(typeface);
+            calendarView.refreshCalendar(currentCalendar);
+        }
+
+        //adding calendar day decorators
+        List decorators = new ArrayList<>();
+        decorators.add(new ColorDecorator());
+        calendarView.setDecorators(decorators);
+        calendarView.refreshCalendar(currentCalendar);*/
+    }
+}
